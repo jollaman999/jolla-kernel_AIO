52875c4261f9796a5c7b860d23ee4df194e71040
jollaman999 <admin@jollaman999.com>
2014-10-05 12:37:19 +0900

    disable_stock_init.sh: Fix comment

--- jolla-kernel/stock/script/disable_stock_init.sh
+++ jolla-kernel/stock/script/disable_stock_init.sh
@@ -1,5 +1,5 @@
 #!/sbin/sh
-# backup the old prima stuff
+# Disable stock init
 
 if [ ! -d /system/etc/init.qcom.post_boot.sh ] ; then
         busybox mv /system/etc/init.qcom.post_boot.sh /system/etc/init.qcom.post_boot.sh_bak
