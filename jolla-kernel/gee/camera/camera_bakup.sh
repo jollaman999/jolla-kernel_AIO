7a500b147ec3e15a3d9fe862aba2b3dc103396fd
jollaman999 <admin@jollaman999.com>
2014-10-05 16:08:53 +0900

    Stock_Camera: Remove lib file rather than move it (Permission issue)

--- jolla-kernel/gee/camera/camera_bakup.sh
+++
@@ -1,8 +0,0 @@
-#!/sbin/sh
-# backup the old mako camera stuff
-
-if [ ! -d /system/lib/hw/camera.qcom.so_bak ] ; then
-        busybox mv /system/lib/hw/camera.qcom.so /system/lib/hw/camera.qcom.so_bak
-fi
-
-return $?
