993655cc26eb5fda3a6aabb6a1d1f5236186d996
jollaman999 <admin@jollaman999.com>
2014-10-05 12:53:00 +0900

    camera_bakup.sh: Fix comment

--- jolla-kernel/gee/camera/camera_bakup.sh
+++ jolla-kernel/gee/camera/camera_bakup.sh
@@ -1,5 +1,5 @@
 #!/sbin/sh
-# backup the old prima stuff
+# backup the old mako camera stuff
 
 if [ ! -d /system/lib/hw/camera.qcom.so_bak ] ; then
         busybox mv /system/lib/hw/camera.qcom.so /system/lib/hw/camera.qcom.so_bak
