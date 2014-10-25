abbc43f0eac85e0255a14017a5a0cbe8d28878ee
jollaman999 <admin@jollaman999.com>
2014-10-25 15:56:16 +0900

    stock: init.qcom.post_boot.sh: Remove power lines

--- jolla-kernel/stock/common/etc/init.qcom.post_boot.sh
+++ jolla-kernel/stock/common/etc/init.qcom.post_boot.sh
@@ -8,23 +8,6 @@ ln -s /dev/erandom /dev/urandom
 rm /dev/hw_random
 ln -s /dev/erandom /dev/hw_random
 
-## power
-echo 0 > /sys/module/pm_8x60/modes/cpu0/retention/idle_enabled
-echo 1 > /sys/module/pm_8x60/modes/cpu1/retention/idle_enabled
-echo 1 > /sys/module/pm_8x60/modes/cpu2/retention/idle_enabled
-echo 1 > /sys/module/pm_8x60/modes/cpu3/retention/idle_enabled
-echo 1 /sys/module/pm_8x60/modes/cpu0/standalone_power_collapse/suspend_enabled
-echo 1 /sys/module/pm_8x60/modes/cpu1/standalone_power_collapse/suspend_enabled
-echo 1 /sys/module/pm_8x60/modes/cpu2/standalone_power_collapse/suspend_enabled
-echo 1 /sys/module/pm_8x60/modes/cpu3/standalone_power_collapse/suspend_enabled
-echo 1 /sys/module/pm_8x60/modes/cpu0/standalone_power_collapse/idle_enabled
-echo 1 /sys/module/pm_8x60/modes/cpu1/standalone_power_collapse/idle_enabled
-echo 1 /sys/module/pm_8x60/modes/cpu2/standalone_power_collapse/idle_enabled
-echo 1 /sys/module/pm_8x60/modes/cpu3/standalone_power_collapse/idle_enabled
-echo 1 /sys/devices/system/cpu/cpu1/online
-echo 1 /sys/devices/system/cpu/cpu2/online
-echo 1 /sys/devices/system/cpu/cpu3/online
-
 ## jollaman999
 echo 270000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 echo 270000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
