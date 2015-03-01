69916441cda83aac202da65804aeeffab0cff78c
jollaman999 <admin@jollaman999.com>
2015-03-01 19:00:35 +0900

    Stock_KK: init.qcom.post_boot.sh: OTA service is already run by init.d support

--- jolla-kernel/stock/common/etc/init.qcom.post_boot.sh
+++ jolla-kernel/stock/common/etc/init.qcom.post_boot.sh
@@ -1,9 +1,5 @@
 #!/system/bin/sh
 
-## jollaman999
-# jolla-kernel OTA service
-/system/bin/sh /system/etc/init.d/00jolla-kernel_ota
-
 echo 320000000 > /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/max_gpuclk
 echo 1 > /sys/module/intelli_plug/parameters/intelli_plug_active
 ##
