b0d6653f6562a38d86a49ca412445b9fff2f2110
jollaman999 <admin@jollaman999.com>
2016-11-16 18:25:59 +0900

    sdcardfs: Remove useless lines and fix blank lines when sdcardfs is already enabled

--- jolla-kernel/bullhead/sdcardfs/sdcardfs_enable_m.sh
+++ jolla-kernel/bullhead/sdcardfs/sdcardfs_enable_m.sh
@@ -1,3 +1,17 @@
 #!/sbin/sh
 
-echo ro.sdcardfs.enable=true >> /system/build.prop
+# Cleanup duplicated
+duplicated="$(grep -ihE 'ro.sdcardfs.enable' '/system/build.prop' | wc -l)";
+if [ "$duplicated" -ge 2 ]; then
+  sed -i '/ro.sdcardfs.enable/d' /system/build.prop
+fi
+
+# Cleanup N
+sed -i '/ro.sys.sdcardfs/d' /system/build.prop
+
+# Enable sdcardfs
+found="$(grep -ihE 'ro.sdcardfs.enable=true' '/system/build.prop' | wc -l)";
+if [ "$found" -eq 0 ]; then
+  sed -i '/ro.sdcardfs.enable/d' /system/build.prop
+  echo ro.sdcardfs.enable=true >> /system/build.prop
+fi
