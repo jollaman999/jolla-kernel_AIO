ad4503bc6d866e5d3dae0e6da0b527e7959ab9af
jollaman999 <admin@jollaman999.com>
2016-12-04 03:19:31 +0900

    Revert "init.d: Also find busybox in /su directory"
    This reverts commit 8fffbdcd8ca6414cf521fc790d318acf1630fe0e.

--- jolla-kernel/bullhead/mkbootimg/init.d_support.sh
+++ jolla-kernel/bullhead/mkbootimg/init.d_support.sh
@@ -13,38 +13,22 @@ find . -iname '*.rc' -maxdepth 1 -type f -exec sed -i '/start.*userinit/d' '{}' 
 
 foundcount="$(find . -iname 'init.rc' -maxdepth 1 -type f -exec grep -ihE 'run\-parts /system/etc/init\.d' '{}' \; | wc -l)";
 if [ "$foundcount" -eq 0 ]; then
-        # find busybox in /system/
+        #find busybox in /system
         bblocation=$(find /system/ -name 'busybox')
-
-        # Use busybox in /su/ directory if exist
-        if [ -e /data/su.img ] ; then
-                mkdir /su/
-                mount -t ext4 -o loop,noatime /data/su.img /su/
-
-                # find busybox in /su/
-                bblocation=$(find /su/ -name 'busybox')
-        fi
-
-
         if [ -n "$bblocation" ] && [ -e "$bblocation" ] ; then
                 echo "BUSYBOX FOUND!";
-                # strip possible leading '.'
+                #strip possible leading '.'
                 bblocation=${bblocation#.};
-
-                if [ -d /su/ ] ; then
-                        umount /su/
-                        rmdir /su/
-                fi
         else
                 echo "NO BUSYBOX NOT FOUND! copy busybox from bin directory.";
                 echo "Setting busybox location to /system/xbin/busybox";
-                # set default location since we couldn't find busybox
+                #set default location since we couldn't find busybox
                 cp "$bin/busybox" /system/xbin/busybox
                 chmod 755 /system/xbin/busybox
                 bblocation="/system/xbin/busybox";
         fi
 
-        # append the new lines for this option at the bottom
+        #append the new lines for this option at the bottom
         echo "" >> init.rc
         echo "service userinit $bblocation run-parts /system/etc/init.d" >> init.rc
         echo "    class late_start" >> init.rc
