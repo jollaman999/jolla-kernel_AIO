7c2754a19ab455249931d95a65b49a0d99112aa1
jollaman999 <admin@jollaman999.com>
2016-12-04 03:21:52 +0900

    init.d: Clean up for any busybox paths

--- jolla-kernel/bullhead/mkbootimg/init.d_cleanup.sh
+++ jolla-kernel/bullhead/mkbootimg/init.d_cleanup.sh
@@ -8,22 +8,22 @@ cd ramdisk;
 
 #### Remove old lines from init.rc ####
 #
-#service userinit /system/xbin/busybox run-parts /system/etc/init.d
+#service userinit [any_path] run-parts /system/etc/init.d
 #    oneshot
 #    class late_start
 #    user root
 #    group root
-sed -i ':a;N;$!ba;s/\nservice userinit \/system\/xbin\/busybox run-parts \/system\/etc\/init.d\n \ \ \ oneshot\n \ \ \ class late_start\n \ \ \ user root\n \ \ \ group root//' init.rc
+sed -i ':a;N;$!ba;s/\nservice userinit.*run-parts \/system\/etc\/init.d\n \ \ \ oneshot\n \ \ \ class late_start\n \ \ \ user root\n \ \ \ group root//' init.rc
 
 #### Remove old lines from init.rc ####
 #
-#service userinit /system/xbin/busybox run-parts /system/etc/init.d
+#service userinit [any_path] run-parts /system/etc/init.d
 #    class late_start
 #    user root
 #    group root
 #    seclabel u:r:init:s0
 #    oneshot
-sed -i ':a;N;$!ba;s/\nservice userinit \/system\/xbin\/busybox run-parts \/system\/etc\/init.d\n \ \ \ class late_start\n \ \ \ user root\n \ \ \ group root\n \ \ \ seclabel u:r:init:s0\n \ \ \ oneshot//' init.rc
+sed -i ':a;N;$!ba;s/\nservice userinit.*run-parts \/system\/etc\/init.d\n \ \ \ class late_start\n \ \ \ user root\n \ \ \ group root\n \ \ \ seclabel u:r:init:s0\n \ \ \ oneshot//' init.rc
 
 echo "Done!";
 exit 0;
