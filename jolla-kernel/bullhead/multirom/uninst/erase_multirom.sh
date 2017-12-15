f9ee04aaa189f02c50e342b04d4ef09be3e9dafd
jollaman999 <admin@jollaman999.com>
2017-12-16 03:51:56 +0900

    171216 snapshopt

--- jolla-kernel/bullhead/multirom/uninst/erase_multirom.sh
+++ jolla-kernel/bullhead/multirom/uninst/erase_multirom.sh
@@ -1,31 +1,44 @@
 #!/sbin/sh
 base=""
-if [ -d "/data/media/multirom" ] ; then
-    base="/data/media/multirom"
-elif [ -d "/data/media/0/multirom" ] ; then
-    base="/data/media/0/multirom"
+if [ -d "/data/media/MultiROM/multirom" ] ; then
+    base="/data/media/MultiROM/multirom"
+elif [ -d "/data/media/0/MultiROM/multirom" ] ; then
+    base="/data/media/0/MultiROM/multirom"
 else
     echo "MultiROM folder was not found"
     exit 0
 fi
 
-/tmp/busybox chattr -R -i "$base"
-
 cd "$base"
+/tmp/busybox chattr -R -i enc
 rm -rf enc
+/tmp/busybox chattr -R -i icons
 rm -rf icons
+/tmp/busybox chattr -R -i res
 rm -rf res
+/tmp/busybox chattr -R -i ubuntu-init
 rm -rf ubuntu-init
+/tmp/busybox chattr -R -i ubuntu-touch-init
 rm -rf ubuntu-touch-init
+/tmp/busybox chattr -R -i ubuntu-touch-sysimage-init
 rm -rf ubuntu-touch-sysimage-init
+/tmp/busybox chattr -R -i adbd
 rm -rf adbd
+/tmp/busybox chattr -R -i busybox
 rm -rf busybox
+/tmp/busybox chattr -R -i exfat-fuse
 rm -rf exfat-fuse
+/tmp/busybox chattr -R -i kexec
 rm -rf kexec
+/tmp/busybox chattr -R -i lz4
 rm -rf lz4
+/tmp/busybox chattr -R -i mrom.fstab
 rm -rf mrom.fstab
+/tmp/busybox chattr -R -i multirom
 rm -rf multirom
+/tmp/busybox chattr -R -i ntfs-3g
 rm -rf ntfs-3g
+/tmp/busybox chattr -R -i trampoline
 rm -rf trampoline
 
 return $?
--- jolla-kernel/bullhead/multirom/uninst/erase_multirom_all.sh
+++ jolla-kernel/bullhead/multirom/uninst/erase_multirom_all.sh
@@ -1,14 +1,29 @@
 #!/sbin/sh
-base=""
+base1=""
 if [ -d "/data/media/multirom" ] ; then
-    base="/data/media/multirom"
+    base1="/data/media/multirom"
 elif [ -d "/data/media/0/multirom" ] ; then
-    base="/data/media/0/multirom"
+    base1="/data/media/0/multirom"
 else
-    echo "MultiROM folder was not found"
+    echo "base1 MultiROM folder was not found"
     exit 0
 fi
 
-/tmp/busybox chattr -R -i "$base"
-rm -rf "$base"
+/tmp/busybox chattr -R -i "$base1"
+rm -rf "$base1"
+
+base2=""
+if [ -d "/data/media/MultiROM" ] ; then
+    base2="/data/media/MultiROM"
+elif [ -d "/data/media/0/MultiROM" ] ; then
+    base2="/data/media/0/MultiROM/"
+else
+    echo "base2 MultiROM folder was not found"
+    exit 0
+fi
+
+/tmp/busybox chattr -R -i "$base2"
+rm -rf "$base2"
+
+
 return $?
